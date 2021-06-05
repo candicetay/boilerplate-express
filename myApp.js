103b6f2c2ffa550213a2a9b434c2af3bc338427d
unknown <candicetayjh@hotmail.sg>
2021-06-05 15:30:27 +0800

    Express tutorial

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  //   res.send("Hello Express");
+  absolutePath = __dirname + "./views/index.html";
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
