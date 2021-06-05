2b160a0cc512c4068bafc2146ad950aa3660be63
unknown <candicetayjh@hotmail.sg>
2021-06-05 15:52:06 +0800

    Express tutorial

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 var express = require("express");
+var path = require("path");
 var app = express();
-console.log(__dirname);
+
 app.get("/", function (req, res) {
   //   res.send("Hello Express");
   absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
-app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 module.exports = app;
