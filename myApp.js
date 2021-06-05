734b28284b98addd977b2ef2b819bb008566cea8
unknown <candicetayjh@hotmail.sg>
2021-06-05 15:25:26 +0800

    Express tutorial

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-  res.send("Response String");
+  res.send("Hello Express");
 });
 
 module.exports = app;
