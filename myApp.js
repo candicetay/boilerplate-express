5e2dc27816c7b1e270467c9105052feec1080abe
unknown <candicetayjh@hotmail.sg>
2021-06-13 15:35:32 +0800

    Root level request logger middleware

--- myApp.js
+++ myApp.js
@@ -6,20 +6,30 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+// Serve an HTML file
 app.get("/", function (req, res) {
   //   res.send("Hello Express");
   absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
+// Serve static assets
 app.use("/public", express.static(path.join(__dirname, "public")));
 console.log(path.dirname(require.main.filename));
-app.get("/json", function (req, res) {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "Hello json" });
-  }
-});
 
+// Use env var
+// app.get("/json", function (req, res) {
+//   if (process.env.MESSAGE_STYLE === "uppercase") {
+//     res.json({ message: "HELLO JSON" });
+//   } else {
+//     res.json({ message: "Hello json" });
+//   }
+// });
+
+// Implement a Root-Level Request Logger Middleware
+// for all paths
+app.use(function (req, res, next) {
+  console.log(req.method, req.path, "-", req.ip);
+  next();
+});
 module.exports = app;
